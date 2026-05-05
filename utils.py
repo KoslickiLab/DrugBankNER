610a8f8a4a1ff2d742377771cd0264524a66e1cd
mohsenht <mohsen.taheri.1989@gmail.com>
2026-05-05 16:37:34 -0400

    Preferred_name error

--- utils.py
+++ utils.py
@@ -40,7 +40,7 @@ def delete_long_tokens(text, max_length=100):
 
 def get_preferred_name(curie, synonymizer):
     results = synonymizer.get_canonical_curies(curies=curie)
-    if results is None or curie not in results or 'preferred_name' not in results[curie]:
+    if results is None or curie not in results or not isinstance(results[curie], dict) or 'preferred_name' not in results[curie]:
         return ""
     return results[curie]['preferred_name']
 
